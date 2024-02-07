4e26398f653f2e2c5ffe9a3dc92a4ac83bb44396
weland <weland@bupt.edu.cn>
2024-02-07 12:52:30 +0800

    define-nier-theme & magit

--- nier-theme.el
+++ nier-theme.el
@@ -38,69 +38,84 @@
      (yorha-cursor ,(if (true-color-p) "#f4f0e1" "#404040" ))
      ))
 
-(custom-theme-set-faces
+(defmacro define-nier-theme (theme &rest faces)
+  `(let ((class '((class color) (min-colors 89)))
+         ,@nier-palette)
+     (custom-theme-set-faces
+      ,theme
+       ,@faces)))
+
+(define-nier-theme
   'nier
   ;;; base
-  '(default                      ((t (:background "#dad4bb" :foreground "#404040"))))
-  '(highlight                    ((t (:background "#999482"))))
-  '(cursor                       ((t (:background "#727969"))))
-  '(custom-comment               ((t (:foreground "#A9A491"))))
-  '(region                       ((t (:extend t :background "#727969"))))
-  '(secondary-selection          ((t (:extend t :background "#f9f5d7"))))
-  '(link                         ((t (:underline t :foreground "#3ba99f"))))
-  '(minibuffer-prompt            ((t (:foreground "#f9f5d7" :background "#686458"))))
-  '(error                        ((t (:foreground "#9d0006" :weight bold))))
-  '(success                      ((t (:foreground "#727b59" :weight bold))))
-  '(match                        ((t (:background "#b4af9a"))))
-  '(isearch                      ((t (:background "#8f3f71" :foreground "#f9f5d7"))))
-  '(custom-set                   ((t (:background "#f9f5d7" :foreground "#282828"))))
-  '(lazy-highlight               ((t (:background "#5e6666" :distant-foreground "black"))))
-  '(show-paren-match             ((t (:background "#b4af9a"))))
-  '(fringe                       ((t (:background "#dad4bb"))))
+  `(default                      ((,class (:background ,yorha-bg :foreground ,yorha-fg))))
+  `(highlight                    ((,class (:background ,ns5))))
+  `(cursor                       ((,class (:background ,ng2))))
+  `(custom-comment               ((,class (:foreground ,ns4))))
+  `(region                       ((,class (:extend t :background ,ng2))))
+  `(secondary-selection          ((,class (:extend t :background ,yorha-0))))
+  `(link                         ((,class (:underline t :foreground ,na3))))
+  `(minibuffer-prompt            ((,class (:foreground ,yorha-0 :background ,ng3))))
+  `(error                        ((,class (:foreground ,yorha-1 :weight bold))))
+  `(success                      ((,class (:foreground ,na4 :weight bold))))
+  `(match                        ((,class (:background ,na2))))
+  `(isearch                      ((,class (:background ,yorha-5 :foreground ,yorha-0))))
+  `(custom-set                   ((,class (:background ,yorha-0 :foreground ,yorha-9))))
+  `(lazy-highlight               ((,class (:background ,ng4 :distant-foreground "black"))))
+  `(show-paren-match             ((,class (:background ,na2))))
+  `(fringe                       ((,class (:background ,yorha-bg))))
 
   ;;; diff
-  '(diff-added                   ((t (:inherit diff-changed :extend t :background "#727b59"))))
-  '(diff-removed                 ((t (:inherit diff-changed :extend t :background "#cd664d"))))
-  '(diff-changed                 ((t (:extend t :background "#bdae93"))))
-  '(diff-hl-change               ((t (:background "#686458" :foreground "#b4af9a"))))
-
+  `(diff-added                   ((,class (:inherit diff-changed :extend t :background ,na4))))
+  `(diff-removed                 ((,class (:inherit diff-changed :extend t :background ,na1))))
+  `(diff-changed                 ((,class (:extend t :background ,yorha-8))))
+  `(diff-hl-change               ((,class (:background ,ng3 :foreground ,na2))))
+  ;; smerge/magit
+  `(smerge-upper ((,class (:extend t :background ,na1))))
+  `(smerge-lower ((,class (:extend t :background ,na4))))
+  `(magit-diff-context-highlight ((,class (:extend t :background ,na2 :foreground ,yorha-fg))))
+  `(magit-diff-added-highlight ((,class (:extend t :background ,na4 :foreground ,yorha-0))))
+  `(magit-diff-removed-highlight ((,class (:extend t :background ,na1 :foreground ,yorha-0))))
+  `(magit-diff-hunk-heading-highlight ((,class (:extend t :background ,ng2 :foreground ,yorha-0))))
+  `(magit-diff-hunk-heading ((,class (:extend t :background ,yorha-bg))))
+  `(magit-section-highlight ((,class (:extend t :background ,ns3))))
 
   ;;; completion
-  '(completions-common-part      ((t (:foreground "#076678"))))
+  `(completions-common-part      ((,class (:foreground ,yorha-4))))
   ;; orderless
-  '(orderless-match-face-2       ((t (:weight bold :foreground "#427b58"))))
-  '(orderless-match-face-1       ((t (:weight bold :foreground "#8f3f71"))))
-  '(orderless-match-face-0       ((t (:weight bold :foreground "#076678"))))
+  `(orderless-match-face-2       ((,class (:weight bold :foreground ,yorha-6))))
+  `(orderless-match-face-1       ((,class (:weight bold :foreground ,yorha-5))))
+  `(orderless-match-face-0       ((,class (:weight bold :foreground ,yorha-4))))
   ;; corfu
-  '(corfu-default                ((t (:background "#cac4ad"))))
-  '(corfu-border                 ((t (:background "#a9a491"))))
-  '(corfu-current                ((t (:foreground "#f9f5d7" :background "#787466"))))
+  `(corfu-default                ((,class (:background ,ns2))))
+  `(corfu-border                 ((,class (:background ,ns4))))
+  `(corfu-current                ((,class (:foreground ,yorha-0 :background "#787466"))))
 
   ;;; popup
-  '(popup-face                   ((t (:inherit default :background "#f9f5d7" :foreground "black"))))
+  `(popup-face                   ((,class (:inherit default :background ,ns1 :foreground ,ng1))))
 
   ;;; telega
-  '(telega-msg-heading           ((t (:background "#b9b49f"))))
+  `(telega-msg-heading           ((,class (:background ,ns3))))
 
   ;;; org
-  '(org-todo                     ((t (:forground "#b26f5f"))))
+  `(org-todo                     ((,class (:forground ,na5))))
 
   ;;; font-lock
-  '(font-lock-comment-face       ((t (:foreground "#A9A491"))))
-  '(font-lock-type-face          ((t (:foreground "#9f8962"))))
-  '(font-lock-builtin-face       ((t (:foreground "#787466"))))
-  '(font-lock-function-name-face ((t (:foreground "#076678"))))
-  '(font-lock-string-face        ((t (:foreground "#427b58"))))
-  '(font-lock-keyword-face       ((t (:foreground "#b26f5f"))))
-  '(font-lock-variable-name-face ((t (:foreground "#5e6666"))))
+  `(font-lock-comment-face       ((,class (:foreground ,ns4))))
+  `(font-lock-type-face          ((,class (:foreground "#9f8962"))))
+  `(font-lock-builtin-face       ((,class (:foreground "#787466"))))
+  `(font-lock-function-name-face ((,class (:foreground ,yorha-4))))
+  `(font-lock-string-face        ((,class (:foreground ,yorha-6))))
+  `(font-lock-keyword-face       ((,class (:foreground ,na5))))
+  `(font-lock-variable-name-face ((,class (:foreground ,ng4))))
 
   ;;; mode-line & misc ui
-  '(mode-line                    ((t (:background "#686458" :foreground "#dad4bb"))))
-  '(mode-line-inactive           ((t (:inherit mode-line :background "#cac4ad" :foreground "grey20" :weight light))))
-  '(widget-field                 ((t (:box (:line-width (1 . -1) :color "gray80") :background "#f4f0e1" :extend t))))
-  '(custom-button                ((t (:box (:line-width (2 . 2) :style released-button) :foreground "#404040" :background "#f4f0e1"))))
-  '(custom-button-mouse          ((t (:background "f9f5d7" :foreground "black" :box (:line-width (2 . 2) :style released-button)))))
-  '(custom-button-pressed        ((t (:box (:line-width (2 . 2) :style pressed-button) :foreground "black" :background "#f9f5d7"))))
+  `(mode-line                    ((,class (:background ,yorha-fg :foreground ,ns1))))
+  `(mode-line-inactive           ((,class (:inherit mode-line :background ,ns2 :foreground "grey20" :weight light))))
+  `(widget-field                 ((,class (:box (:line-width (1 . -1) :color "gray80") :background ,yorha-cursor :extend t))))
+  `(custom-button                ((,class (:box (:line-width (2 . 2) :style released-button) :foreground ,ng1 :background ,yorha-cursor))))
+  `(custom-button-mouse          ((,class (:background "f9f5d7" :foreground "black" :box (:line-width (2 . 2) :style released-button)))))
+  `(custom-button-pressed        ((,class (:box (:line-width (2 . 2) :style pressed-button) :foreground "black" :background ,yorha-0))))
   )
 
 (provide-theme 'nier)
