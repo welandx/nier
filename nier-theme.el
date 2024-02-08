dc2f034463efd22d64145e6febb95416ac709395
weland <weland@bupt.edu.cn>
2024-02-08 19:16:52 +0800

    tab-bar org

--- nier-theme.el
+++ nier-theme.el
@@ -6,8 +6,8 @@
     (= (tty-display-color-cells) 16777216)))
 
 (defconst nier-palette
-  `((ng1           ,(if (true-color-p) "#404040" "#585858"))
-     (ng2          ,(if (true-color-p) "#787466" "#585858"))
+  `((ng1           ,(if (true-color-p) "#404040" "#444"))
+     (ng2          ,(if (true-color-p) "#787466" "#776"))
      (ng3          ,(if (true-color-p) "#686458" "#303030"))
      (ng4          ,(if (true-color-p) "#5e6666" "#303030"))
      (ng5          ,(if (true-color-p) "#50403c" "#303030"))
@@ -36,6 +36,11 @@
      (yorha-bg     ,(if (true-color-p) "#c2bda6" "#404040" ))
      (yorha-fg     ,(if (true-color-p) "#48463d" "#404040" ))
      (yorha-cursor ,(if (true-color-p) "#f4f0e1" "#404040" ))
+     (nier1        ,(if (true-color-p) "#F4D9AF" "#202020"))
+     (nier2        ,(if (true-color-p) "#6f414c" "#202020"))
+     (nier3        ,(if (true-color-p) "#c1a898" "#202020"))
+     (nier4        ,(if (true-color-p) "#cab49e" "#202020"))
+     (nier5        ,(if (true-color-p) "#77713F" "#202020"))
      ))
 
 (defmacro define-nier-theme (theme &rest faces)
@@ -50,26 +55,30 @@
   ;;; base
   `(default                      ((,class (:background ,yorha-bg :foreground ,yorha-fg))))
   `(highlight                    ((,class (:background ,ns5))))
-  `(cursor                       ((,class (:background ,ng2))))
-  `(custom-comment               ((,class (:foreground ,ns4))))
-  `(region                       ((,class (:extend t :background ,ng2))))
+  `(cursor                       ((,class (:background ,nier5))))
+  `(custom-comment               ((,class (:foreground "#b7bd93"))))
+  `(region                       ((,class (:extend t :background "#9a916a"))))
   `(secondary-selection          ((,class (:extend t :background ,yorha-0))))
-  `(link                         ((,class (:underline t :foreground ,na3))))
+  `(link                         ((,class (:underline t :foreground ,yorha-5))))
   `(minibuffer-prompt            ((,class (:foreground ,yorha-0 :background ,ng3))))
   `(error                        ((,class (:foreground ,yorha-1 :weight bold))))
   `(success                      ((,class (:foreground ,na4 :weight bold))))
+  `(warning                      ((,class (:foreground ,yorha-3 :weight bold))))
   `(match                        ((,class (:background ,na2))))
   `(isearch                      ((,class (:background ,yorha-5 :foreground ,yorha-0))))
   `(custom-set                   ((,class (:background ,yorha-0 :foreground ,yorha-9))))
   `(lazy-highlight               ((,class (:background ,ng4 :distant-foreground "black"))))
   `(show-paren-match             ((,class (:background ,na2))))
   `(fringe                       ((,class (:background ,yorha-bg))))
-  `(help-key-binding                       ((,class (:background ,ns1 :box (:line-width (1 . -1) :color ,ng4)))))
+  `(help-key-binding                       ((,class (:background ,ns1 :foreground ,yorha-9 :box (:line-width (1 . -1) :color ,ng4)))))
   `(header-line                       ((,class (:background ,ns3 :foreground ,yorha-9 :box nil :inherit mode-line))))
   ;;whitespace
   `(whitespace-empty                       ((,class (:extend t :background ,yorha-8))))
+  `(whitespace-trailing                       ((,class (:bold t :background ,yorha-8 :foreground ,yorha-3))))
 
   ;;; diff
+  `(diff-file-header                   ((,class (:extend t :background ,nier2))))
+  `(diff-header                   ((,class (:extend t :background ,ns5))))
   `(diff-added                   ((,class (:inherit diff-changed :extend t :background ,na4))))
   `(diff-removed                 ((,class (:inherit diff-changed :extend t :background ,na1))))
   `(diff-changed                 ((,class (:extend t :background ,yorha-8))))
@@ -105,16 +114,20 @@
   `(telega-msg-heading           ((,class (:background ,ns3))))
 
   ;;; org
-  `(org-todo                     ((,class (:forground ,na5))))
+  `(org-todo                     ((,class (:foreground ,na5))))
+  `(org-table                     ((,class (:foreground ,nier2))))
+  `(org-modern-done ((,class (:background ,yorha-8 :foreground ,yorha-9 :inherit org-modern-label))))
 
   ;;; font-lock
-  `(font-lock-comment-face       ((,class (:foreground ,ns4))))
+  `(font-lock-comment-face       ((,class (:foreground ,ns5))))
   `(font-lock-type-face          ((,class (:foreground "#9f8962"))))
   `(font-lock-builtin-face       ((,class (:foreground "#787466"))))
-  `(font-lock-function-name-face ((,class (:foreground ,yorha-4))))
-  `(font-lock-string-face        ((,class (:foreground ,yorha-6))))
+  `(font-lock-function-name-face ((,class (:foreground ,nier5))))
+  `(font-lock-string-face        ((,class (:foreground ,na4))))
   `(font-lock-keyword-face       ((,class (:foreground ,na5))))
   `(font-lock-variable-name-face ((,class (:foreground ,ng4))))
+  `(font-lock-constant-face ((,class (:foreground ,yorha-4))))
+  `(font-lock-warning-face ((,class (:inherit error :bold nil))))
 
   ;;; mode-line & misc ui
   `(mode-line                    ((,class (:background ,yorha-fg :foreground ,ns1))))
@@ -123,6 +136,10 @@
   `(custom-button                ((,class (:box (:line-width (2 . 2) :style released-button) :foreground ,ng1 :background ,yorha-cursor))))
   `(custom-button-mouse          ((,class (:background "f9f5d7" :foreground "black" :box (:line-width (2 . 2) :style released-button)))))
   `(custom-button-pressed        ((,class (:box (:line-width (2 . 2) :style pressed-button) :foreground "black" :background ,yorha-0))))
+  ;;tab-bar
+  `(tab-bar ((,class (:background ,yorha-bg :inherit variable-pitch))))
+  `(tab-bar-tab ((,class (:background ,ns1 :inherit tab-bar))))
+  `(tab-bar-tab-inactive ((,class (:background ,ns3 :inherit tab-bar))))
   )
 
 (provide-theme 'nier)
